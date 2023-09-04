7c3d3283234cf22b234d89d081c0de7892b42a5a
John Sprague <spraguejdev@gmail.com>
2023-09-04 16:36:32 -0400

    chore: add title and animation

--- john-sprague-portfolio/src/app/page.tsx
+++ john-sprague-portfolio/src/app/page.tsx
@@ -25,10 +25,16 @@ export default function Home() {
 						>
 							{item.name}
 						</Link>
-					))}
+))}
 				</ul>
 			</nav>
+			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 
+			<h1 className=" p-10 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+				John Sprague
+			</h1>
+
+			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 		</div>
   )
 }
